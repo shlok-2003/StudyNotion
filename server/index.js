f9af2690511cfe648292fd1caf99df09c6d6f19b
Shlok Prajapati <shlokprajapati2003@gmail.com>
2023-11-11 18:10:09 +0530

    Updated code-structure

--- server/index.js
+++ server/index.js
@@ -1,35 +1,29 @@
-import express from 'express';
 import dotenv from 'dotenv';
-
-//? Custom dependencies
-import dbConnect from "./config/database.js";
-import userRouter from './routes/user.js';
 
 //* Development dependencies
 import chalk from 'chalk';
 
+//? Custom dependencies
+import dbConnect from './config/database.js';
+import cloudConnect from './config/cloudinary.js';
+
+//! Importing app
+import app from './app.js';
+
 //? Requirements
 dotenv.config();
-const app = express();
 const PORT = process.env.PORT || 4001;
-
-//? Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use('/user', userRouter)
-
-//? Routes
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
 
 //? Server
 app.listen(PORT, (err) => {
     if (err) {
-        console.error("Error in starting server");
-        return console.log(chalk.red(`Error in starting server: ${err}`));
+        console.error('Error in starting server');
+        console.log(chalk.red(`Error in starting server: ${err}`));
+        process.exit(1);
     }
 
-    dbConnect();    
+    dbConnect();
+    cloudConnect();
+
     console.log(chalk.blue(`Server running on port ${PORT}`));
-})
+});
