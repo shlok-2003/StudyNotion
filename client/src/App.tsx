ccb63c861ecce7e07eb081eaaba85dda9b2196e8
shlok-2003 <shlokprajapati2003@gmail.com>
2024-02-10 17:36:00 +0530

    Added theme-provider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,8 @@
-import { 
-    Route,
-    RouterProvider, 
-    createBrowserRouter,
-    createRoutesFromChildren
-} from 'react-router-dom'
-import './styles/App.css'
-
-import Main from './layouts/Main.tsx'
-import { Home, NotFound } from './pages'
-import Error from './components/error/index.tsx'
-
-const router = createBrowserRouter(
-    createRoutesFromChildren(
-        <Route path='/' element={<Main/>} errorElement={<Error />} >
-            <Route index element={<Home/>} />
-            <Route path='*' element={<NotFound/>} />
-        </Route>
-    )
-)
-
 export default function App() {
+
     return (
-        <RouterProvider router={router} />
-    )
+        <>
+            Hello;
+        </>
+    );
 }
