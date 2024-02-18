358e854266a1532e6516190cdaf555d3b5d3a1ca
shlok-2003 <shlokprajapati2003@gmail.com>
2024-02-18 17:15:04 +0530

    Added axios instances

---
+++ client/src/api/index.ts
@@ -0,0 +1,9 @@
+const BASE_URL = 'http://localhost:4000/api';
+
+export const review = {
+    REVIEW_API: BASE_URL + '/course/get-rating',
+};
+
+export const contactUs = {
+    CONTACT_US_API: BASE_URL + '/reach/contact',
+};
