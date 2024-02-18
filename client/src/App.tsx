a7499799b47b0931bbdead9510f72c1d8aa5f455
shlok-2003 <shlokprajapati2003@gmail.com>
2024-02-18 17:21:55 +0530

    Updated and added neccessary Props for redux and toaster

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,32 @@
-export default function App() {
+import {
+    Route,
+    RouterProvider,
+    createBrowserRouter,
+    createRoutesFromChildren,
+} from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
+import Main from '@/layouts/Main';
+import { About, Home, Contact, NotFound } from '@/pages';
+import React from 'react';
+
+const router = createBrowserRouter(
+    createRoutesFromChildren(
+        <Route path="/" element={<Main />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+
+            <Route path="*" element={<NotFound />} />
+        </Route>,
+    ),
+);
+
+export default function App() {
     return (
-        <>
-            Hello;
-        </>
+        <React.Fragment>
+            <ToastContainer position="bottom-right" />
+            <RouterProvider router={router} />
+        </React.Fragment>
     );
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { ThemeProvider } from '@/components/theme/theme-provider.tsx';
 import App from './App.tsx';
+// import { ThemeProvider } from '@/components/theme/theme-provider.tsx';
+import { Provider } from 'react-redux';
+import store from '@/redux';
 import './styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+        <Provider store={store}>
             <App />
-        </ThemeProvider>
+        </Provider>
     </React.StrictMode>,
 );
+
+{
+    /* <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme"> */
+}
+{
+    /* </ThemeProvider> */
+}
