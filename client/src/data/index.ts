4319ccec0d30b2381204cb8ddd0aa76121c1568b
shlok-2003 <shlokprajapati2003@gmail.com>
2024-02-20 01:45:43 +0530

    enabled default export

---
+++ client/src/data/index.ts
@@ -0,0 +1,6 @@
+export { default as LessonData } from './lesson-card';
+export { default as CodeData } from './code-block';
+export { default as sidebarLinks } from './dashboard-link';
+export { default as QnaData } from './qna';
+export { default as LearningGridData } from './learning-grid';
+export { default as ContactData } from './contact-details';
